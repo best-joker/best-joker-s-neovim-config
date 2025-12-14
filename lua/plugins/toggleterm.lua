7221fe25eb9847eb8b4a480c55689977e7d15712
无敌joker <17775369906@163.com>
2025-12-14 14:23:04 +0800

    config:终端位置尝试改变

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,6 +6,8 @@ end
 toggleterm.setup({
   size = 15,
   open_mapping = [[<C-\>]],
+  -- direction = "horizontal",类似于vscode的底部终端
+  -- direction = "vertical",
   direction = "float",
   shade_terminals = true,
   shell = "pwsh",  -- 使用 PowerShell 7
